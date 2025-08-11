0657faf2f9bf05347737fa469d9b73072cebe007
Nuriddin Mahmud <154328556+nuriddinmahmud@users.noreply.github.com>
2025-08-11 11:58:40 +0500

    Fix: wishlist slice import path & typed hooks

--- src/hooks.ts
+++
@@ -1,8 +0,0 @@
-import { useDispatch, useSelector } from "react-redux";
-import type { RootState, AppDispatch } from "./app/store";
-
-export const useAppSelector = <TSelected>(
-  selector: (state: RootState) => TSelected
-) => useSelector(selector);
-
-export const useAppDispatch = () => useDispatch<AppDispatch>();
